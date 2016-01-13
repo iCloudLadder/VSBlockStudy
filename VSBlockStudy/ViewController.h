590bbdddd6239c338a2f7dec6142c47ba6e8ab42
weishiyue <w_shiyue@163.com>
2016-01-13 09:25:05 +0800

    Initial Commit

---
+++ VSBlockStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VSBlockStudy
+//
+//  Created by cooperLink on 16/1/13.
+//  Copyright © 2016年 VS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VSBlockStudy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VSBlockStudy
+//
+//  Created by cooperLink on 16/1/13.
+//  Copyright © 2016年 VS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
